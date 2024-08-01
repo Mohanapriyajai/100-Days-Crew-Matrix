96535a9db6d61206c870bff232bda73a2d12948e
S.Mohanapriya <mohanapriyajaikumar2000@gmail.com>
2024-08-01 20:59:08 +0530

    Multiply 2 numbers
    1)Multiplying integers
    2)Multiplying floating-points numbers

---
+++ day one.py
@@ -0,0 +1,11 @@
+# Multiplying Integers
+num1 = int(input("Please Enter the First Number: "))
+num2 = int(input("Please Enter the Second Number: "))
+product = num1 * num2
+print(f"The Result of Multiplying {num1} and {num2} = {product}")
+
+# Multiplying Floating-Point Numbers
+num1 = float(input("Please Enter the First Number: "))
+num2 = float(input("Please Enter the Second Number: "))
+product = num1 * num2
+print(f"The Result of Multiplying {num1} and {num2} = {product}")
